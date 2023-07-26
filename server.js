14c2e60ee3be2bb9b069024f57b730ab81d7f02d
jas-rod <jas.rod121@gmail.com>
2023-07-26 15:47:38 +0000

    Changing settings

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 let express = require('express'),
     request = require('request'),
+    cors = require('cors');
     app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳');
 });
 
 app.get('/api', function (req, res, next) {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
